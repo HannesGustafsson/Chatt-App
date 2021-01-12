4699785fbf787f70ec2c4f8dfc98a5f278aa6c90
DatNisse <adde.press@gmail.com>
2021-01-12 16:35:27 +0100

    Created csharp test client

---
+++ Cstestclient/Cstestclient/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net; //added
+using System.Net.Sockets; //added
+using Google.Protobuf;
+
+namespace Cstestclient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendMessage();
+            //GetMessage(DateTime.Now.Ticks);
+            //GetMessage(0);
+            while (true)
+            {
+
+            }
+        }
+
+        static void SendMessage()
+        {
+            BackendRequest request = new BackendRequest();
+
+            request.IsInput = true;
+            request.Input = new InputMessage()
+            {
+                IpAddress = Dns.GetHostAddresses(Dns.GetHostName()).ToString(),
+                MessageToInput = new MessageObject()
+                {
+                    MessageText = "Test text",
+                    Timestamp = DateTime.Now.Ticks
+                }
+            };
+
+            TcpClient client = new TcpClient("GamePC", 11000);
+            NetworkStream stream = client.GetStream();
+
+            request.WriteTo(stream);
+            Console.WriteLine("Got message");
+
+            
+            client.Close();
+        }
+
+        static void GetMessage(Int64 timestamp)
+        {
+            BackendRequest request = new BackendRequest();
+            GetMessagesResponse response = new GetMessagesResponse();
+            request.IsInput = false;
+            request.Timestamp = timestamp;
+            TcpClient client = new TcpClient("GamePC", 11000);
+            NetworkStream stream = client.GetStream();
+
+            request.WriteTo(stream);
+            client.Close();
+
+            client = new TcpClient("GamePC", 11000);
+            stream = client.GetStream();
+
+            response = GetMessagesResponse.Parser.ParseFrom(stream);
+
+            client.Close();
+        }
+
+
+    }
+}
